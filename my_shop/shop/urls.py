cc456bf3855c58af6f3665e7773701de30fc8da2
pmilosz99 <piter31@poczta.onet.pl>
2022-01-16 20:40:36 +0100

    add urls

--- my_shop/shop/urls.py
+++ my_shop/shop/urls.py
@@ -1,3 +1,4 @@
+from django.urls import path, include
 from rest_framework import routers
 
 from my_shop.shop.views import CategoryViewSet, ProductViewSet
@@ -5,3 +6,7 @@ from my_shop.shop.views import CategoryViewSet, ProductViewSet
 router = routers.DefaultRouter()
 router.register(r'category', CategoryViewSet, basename='category')
 router.register(r'product', ProductViewSet, basename='product')
+
+urlpatterns = [
+    path('shop/', include(router.urls)),
+]
