3aa70c97b504d28542bc14c8f1442772e704cfdb
pmilosz99 <piter31@poczta.onet.pl>
2022-01-30 19:17:18 +0100

    finish app cart and add app orders, create models in orders

--- my_shop/my_shop/urls.py
+++ my_shop/my_shop/urls.py
@@ -22,8 +22,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cart/', include(('cart.urls', 'cart'), namespace='cart')),
     path('', include(('shop.urls', 'shop'), namespace='shop')),
-    path('cart/', include(('cart.urls', 'cart'), namespace='cart'))
 ]
 
 if settings.DEBUG:
